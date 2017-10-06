ff54d4e8c6cc181d49e028ed0c62d1f177e89c86
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-05 18:42:40 -0600

    added omdb API info, wrote an axios get request

--- peanut-gallery/server/index.js
+++ peanut-gallery/server/index.js
@@ -5,10 +5,16 @@ const express = require('express'),
       port = 4000,
       baseURL = `/api/reviews`;
 
+// omdb api 
 let omdbApiKey = '1197693b',
+    omdbBaseURL = 'http://www.omdbapi.com/',
     hardCodedTitle = 'Titanic';
 
+// replace movie title's spaces with %20
+hardCodedTitle = hardCodedTitle.toLowerCase().replace(' ', '%20');
 app.use(bodyParser());
 
+axios.get(`${omdbBaseURL}?apikey=${omdbApiKey}&t=${hardCodedTitle}`)
+     .then((res) => console.log(res.data));
 
 app.listen(port, () => console.log(`I'm listening... on port ${port}`));
