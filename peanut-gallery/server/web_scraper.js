0ecf664c6289375733677774a1c60f654fe0a451
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-10 12:13:45 -0600

    commented web_scraper.js

--- peanut-gallery/server/web_scraper.js
+++ peanut-gallery/server/web_scraper.js
@@ -1,28 +1,33 @@
 const axios = require('axios'),
       cheerio = require('cheerio');
 
+// set progress object to defaults
 let progress = {percent: 0, display:''};
 
 // export the scraping function
 module.exports = {
+  // resets progress object
   refresh: function() {
     progress = {percent: 0, display:''};
   },
   scrapeFromURL: async function (url, title) {
-    // hardcode max num of pages to scrape
+    // hardcoded max num of pages for testing phase
     let lastPageIndex = 10;
     let reviewsHTML = '';
     let reviewsTXT = '';
     let reviewsJSON = '';
     progress.display = `Scraping Page 0 of ${lastPageIndex}`;
+    // cycle through ?start=0 to ?start=numreviews, incrementing by 10
     for (var i = 0; i < lastPageIndex * 10; i += 10) {
       await axios.get(url + `?start=${i}`)
+          // build out html string
           .then((res) => reviewsHTML += res.data)
-          // update progress var each time a review .html page is scraped
+          // update progress vars each time a review .html page is scraped
           .then(() => progress.percent += (1/lastPageIndex) * 100)
           .then(() => progress.display = `Scraping Page ${i/10 + 1} of ${lastPageIndex}`)
           .then(() => {
             let $ = cheerio.load(reviewsHTML);
+            // pull out all text contained in <p> tags and build out txt string
             $('p').each((index, element) => {
               reviewsTXT += $(element).text();
             });
@@ -31,6 +36,7 @@ module.exports = {
     // reset progress after scraping
     return {reviewsTXT: reviewsTXT, reviewsHTML: reviewsHTML};
   },
+  // returns progress object 
   getProgress: function (req, res, next) {
     res.status(200).json(progress);
   }
